8537e68fcf26d2b3c1c1d5c984940aa7820237a0
Annie Choi <36435017+aachoi@users.noreply.github.com>
2018-09-06 23:06:24 -0700

    update given pmid- check for mesh.r

--- given PMIDs- check for MeSH/Given PMID - Check for Journal MeSH.R
+++ given PMIDs- check for MeSH/Given PMID - Check for Journal MeSH.R
@@ -4,13 +4,30 @@
 #         Find all the PMIDS / MH under that journal name
 
 library(rentrez)
+library(rjson)
 library(dplyr)
 library(tidyr)
 library(stringr)
 library(tm)
-library(rjson)
 library(quanteda)
+library(stringi)
 options(stringsAsFactors = FALSE)
+
+
+# ANOMALY-------------
+length(extract_from_esummary(multi_summary, "pubtype"))
+extract_from_esummary(multi_summary, "pubtype")
+multi_summary$'28477389'$pubtype
+
+multi_summary$'28338219'$pubtype <- "Case Report"
+
+summary_results <- bind_rows(summary_results,tibble(PMID = names(unlist(extract_from_esummary(multi_summary, "source"))), AbbrevJournalName = unlist(extract_from_esummary(multi_summary, "source")),FullJournalName = unlist(extract_from_esummary(multi_summary, "fulljournalname")),NLMUniqueID = unlist(extract_from_esummary(multi_summary, "nlmuniqueid")),PubType = unlist(extract_from_esummary(multi_summary, "pubtype"))))
+# Remove anomaly 
+omit <- c(28338219)
+# Index of PMIDS to omit
+omit_index <- which(pmid_full_unique %in% omit)
+pmid_full_unique <- pmid_full_unique[-omit_index]
+# --------------------
 
 # Exploring rentrez package
 #************************************************************************
@@ -24,156 +41,85 @@ entrez_db_searchable("pubmed")
 entrez_db_searchable("nlmcatalog")
 #************************************************************************
 
-search_pmid_full <- as.numeric(fromJSON(file="/Users/Annie/Desktop/nomesh_pmid.json"))
+pmid_full <- as.numeric(fromJSON(file="/Users/Annie/Desktop/nomesh_pmid.json"))
+# Remove duplicate PMIDS
+pmid_full_unique <- unique (pmid_full)
+
+
+#------------------------
+# create trial chunk
+#trial <- pmid_full_unique[1:1400]                                    #CHANGE CHUNK
+#------------------------
 
 # Chunk the PMIDS for smaller requests
-chunk_size <- 250
-pmids_chunked <- split(search_pmid_full, ceiling(seq_along(search_pmid_full)/chunk_size))
+chunk_size <- 199
+pmids_chunked <- split(pmid_full_unique, ceiling(seq_along(pmid_full_unique)/chunk_size))
 
-#our trial pmid chunk
-pmids_chunked[[1]]
-
-#GOAL:
-# Find the type of publication
-# Find the journal name
+#GOAL 1:
+# Find the Abbreviated Journal Name
+# Obtain the distinct Journal Names
 
 # Multiple PMIDS
-# Create the index for the given list of PMIDS
-search_pmid <- c(30145740, 30099694,30176007)
-# Obtain the summary records back of each PMIDS
-multi_summary <- entrez_summary (db = "pubmed", id = search_pmid)
-# Create a tibble with columns PMID, Abbrev Journal Name, Full Journal Name, Publication Type, NLM ID
-summary_results <- tibble (PMID = search_pmid, AbbrevJournalName = extract_from_esummary(multi_summary, "source"), FullJournalName = extract_from_esummary(multi_summary, "fulljournalname"), PublicationType = as.list(as.data.frame(extract_from_esummary(multi_summary, "pubtype"), stringsAsFactors = FALSE)[2,]),NLMUniqueID = extract_from_esummary(multi_summary, "nlmuniqueid"))
-
-#---DO NOT RUN---
-#Explaining the logic behind obtaining publication type
-# We know that we can obtain publication type like so,
-PublicationType = extract_from_esummary(multi_summary, "pubtype")
-str(PublicationType) # But it returns a list where each section of the list is an atomic vector of length 2 - problematic
-PublicationType[[2]]  # We only want the second element each vector of the list
-temp <-  as.data.frame(PublicationType) # We coerce into a dataframe and only get the second row
-temp[2,]
-# Then we have to wrap it all as a list so that it fits into our tibble
-# We condense all of it into one line of code so that we don't waste memory saving names
-temp2 <- as.list(as.data.frame(PublicationType, stringsAsFactors = FALSE)[2,])
-str(temp2)
-#FINAL
-PublicationType = as.list(as.data.frame(extract_from_esummary(multi_summary, "pubtype"), stringsAsFactors = FALSE)[2,])
-#---------------
+summary_results <- tibble()
+for(i in 1:length(pmids_chunked)){
+  # Obtain the summary records back of each PMIDS
+  multi_summary <- entrez_summary (db = "pubmed", id = pmids_chunked[[i]])
+  # Condense strings in PubType into one string
+  for (j in 1: length(extract_from_esummary(multi_summary, "pubtype"))){
+    if(length(multi_summary[[j]]$pubtype) >=2){
+      multi_summary[[j]]$pubtype <- paste(multi_summary[[j]]$pubtype, collapse = " ")
+    }
+    # If PubType has an empty list " list()" , then replace with "Not Available"
+    if(is.list(multi_summary[[j]]$pubtype) & length(multi_summary[[j]]$pubtype) == 0){
+      multi_summary[[j]]$pubtype <- "Not Available"
+    }
+  }
+  # Create a tibble with columns PMID, Abbrev Journal Name, Full Journal Name, NLM ID
+  summary_results <- bind_rows(summary_results,tibble(PMID = names(unlist(extract_from_esummary(multi_summary, "source"))), AbbrevJournalName = unlist(extract_from_esummary(multi_summary, "source")),FullJournalName = unlist(extract_from_esummary(multi_summary, "fulljournalname")),NLMUniqueID = unlist(extract_from_esummary(multi_summary, "nlmuniqueid")),PubType = unlist(extract_from_esummary(multi_summary, "pubtype"))))
+}
+#
+# Obtain all the distinct AbbrevJournalNames
+distinct_summary_results <- distinct(summary_results, AbbrevJournalName, .keep_all = TRUE)
 
 # Reformatting abbreviated journal name to format AbbrevJournalName[JOUR] so that it searches on PubMed
-summary_results$AbbrevJournalName
-summary_results$AbbrevJournalName <- paste (summary_results$AbbrevJournalName, "[JOUR]", sep = "")
+distinct_summary_results$AbbrevJournalName <- paste (distinct_summary_results$AbbrevJournalName, "[JOUR]", sep = "")
 
-
-
-
-
-
-
-# GOAL: 
-# Obtain all the distinct NLM IDS
+# GOAL 2: 
 # Use NLM ID to find all publications under the journal name
-# Determine if any of them have MeSH terms
+# Determine if any of the publications have MeSH terms
 
-# Functions
-# Transforms raw strings pattern to Colon
-toColon <- content_transformer(function(x, pattern) {return (gsub(pattern, "; ", x))})
-# Detects pattern match in raw string
-detect_match <- content_transformer(function(x, pattern){return (str_detect(x, pattern))})
-
-
-# Distinct AbbrevJournalName DataFrame
-distinct_journal_name <- distinct(summary_results,AbbrevJournalName)
-
+# Create a MH_match column in distinct_summary_results
+distinct_summary_results$MH_match = rep(TRUE, length(distinct_summary_results$PMID))
 
 # For Loop for to find PMIDs of each distinct NLM IDs
-for(i in 1:length(distinct_journal_name$AbbrevJournalName)){
+for(i in 1:10){
   # Use AbbrevJournalName to search for the PMIDS 
-  journal_pmid <- entrez_search(db = "pubmed", term = distinct_journal_name$AbbrevJournalName[i], retmax = 99999)$ids
-  
-  
-  
+  journal_pmid <- entrez_search(db = "pubmed", term = distinct_summary_results$AbbrevJournalName[i], retmax = 99999, use_history = TRUE)
+  documents <- Corpus(VectorSource(entrez_fetch (db = "pubmed", web_history = journal_pmid$web_history, rettype = "medline", retmode = "text")))
+  # Detect MH match
+  if (is.na(stri_match_first_regex(documents$content, "\nMH "))) {
+    distinct_summary_results$MH_match[i] <- FALSE
+  }
 }
 
-journal_pmid <- entrez_search(db = "pubmed", term = distinct_journal_name$AbbrevJournalName[2], retmax = 99999)$ids
+# GOAL 3: 
+# Obtain the Journals that don't have MeSH terms
+# Find the corresponding Publisher and Country of Publication
 
-journal_pmid_1 <- journal_pmid[1:400]
-documents <- Corpus(VectorSource(entrez_fetch (db = "pubmed", id = journal_pmid_1, rettype = "medline", retmode = "text")))
-writeLines(as.character(documents[[1]]))
-# Clean Strings
-temp <- tm_map(documents, toColon, "\n")
-temp2 <- tm_map(temp, detect_match, "(MH .+?);")
-# Detect PMID regexr : (PMID.+?);
-temp2[[1]][1]
+# Only retrieve rows that do NOT have MH match
+no_mesh_final_results <- filter(distinct_summary_results, MH_match == FALSE)
 
-
-
-
-
-
-# Use these IDS to analyze mesh terms
-# Have to find a way to efficiently cycle through every single journal
-
-
-
-
-
-
-# GOAL: 
-# Use NLM ID to find the Publisher and Country of Publication
-
-# NLM ID
-# Create the index for the given list of NLMID
-search_nid <- 101590450
 # Obtain the summary records back of each NLMID
-nid_multi_summary <- entrez_summary (db = "nlmcatalog", id = nid)
-# Create a tibble with columns NLMID, Publisher, Country of Publication
-# Country of publication
-nid_multi_summary$country
-# Publisher
-nid_multi_summary$publicationinfolist$publisher 
+no_mesh_final_multi_summary <- entrez_summary (db = "nlmcatalog", id = no_mesh_final_results$NLMUniqueID)
 
-summary_results <- tibble (PMID = search_pmid, FullJournalName = extract_from_esummary(multi_summary, "fulljournalname"), PublicationType = as.list(as.data.frame(extract_from_esummary(multi_summary, "pubtype"), stringsAsFactors = FALSE)[2,]),NLMUniqueID = extract_from_esummary(multi_summary, "nlmuniqueid"))
+# Add columns Publisher and Country of Publication to our final table
+# Initialize columns
+no_mesh_final_results$Publisher <- rep(NA, length(no_mesh_final_results$PMID))
+no_mesh_final_results$CountryOfPublication <- rep(NA, length(no_mesh_final_results$PMID))
+# Add data to columns
+for (i in 1:length(no_mesh_final_results$PMID)){
+  no_mesh_final_results$Publisher[i] <- no_mesh_final_multi_summary[[i]]$publicationinfolist$publisher 
+  no_mesh_final_results$CountryOfPublication[i] <- no_mesh_final_multi_summary[[i]]$country 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-r_search <- entrez_search(db = "pubmed", term = "Mesh term")
-r_search$ids #outputs PMIDS
-
-#eutils api 
-entrez_fetch() #obtains full records in varying formats
-summary_info <- entrez_summary(db = "pubmed", id = 24555091) #returns less information about each record in a simple format
-summary_info #summary info behaves like a list
-#we would be interested in publishername, pubtype, fulljournalname
-rentrez #parse and summarise summary recores
-extract_from_summary(summary_info, "fulljournalname")
-
-
-
-
-
-
-
-
-
-
-
-#single pmid 
-trial <- entrez_summary(db = "pubmed", id = 30145740)
-trial$uid #PMID
-trial$pubtype # publication type
-trial$fulljournalname  #journal name
-trial$publishername 
-trial$nlmuniqueid
+write.csv(no_mesh_final_results, file = "No MH Match Final Results.csv")
